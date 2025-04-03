a85741b4122db93eacdfb238b2f0405a45014e11
Leonardo Rocha Dias <leonardorochadias@gmail.com>
2025-04-03 05:25:50 -0300

    fix rules dictionary to beats 7 moves for RPS-15 requirement

--- RPSGame.Domain/Rules/MoveRules.cs
+++ RPSGame.Domain/Rules/MoveRules.cs
@@ -11,27 +11,27 @@ namespace RPSGame.Domain.Rules
             _rules = new Dictionary<string, List<string>>
             {
                 // Easy Moves
-                { "Rock", new List<string> { "Scissors", "Lizard" } },
-                { "Paper", new List<string> { "Rock", "Spock" } },
-                { "Scissors", new List<string> { "Paper", "Lizard" } },
+                { "Rock", new List<string> { "Scissors", "Lizard", "Sponge", "Fire", "Snake", "Human", "Wolf" } },
+                { "Paper", new List<string> { "Rock", "Spock", "Dragon", "Tree", "Water", "Air", "Lightning" } },
+                { "Scissors", new List<string> { "Paper", "Lizard", "Dragon", "Snake", "Human", "Tree", "Sponge" },
 
                 // Intermediate Moves
-                { "Lizard", new List<string> { "Spock", "Paper" } },
-                { "Spock", new List<string> { "Scissors", "Rock" } },
+                { "Lizard", new List<string> { "Spock", "Paper", "Tree", "Air", "Water", "Dragon", "Wolf" } },
+                { "Spock", new List<string> { "Scissors", "Rock", "Fire", "Lightning", "Gun", "Dragon", "Snake" } },
 
                 // Hard Moves
-                { "Gun", new List<string> { "Rock", "Devil", "Air" } },
-                { "Lightning", new List<string> { "Water", "Sponge", "Devil" } },
-                { "Devil", new List<string> { "Sponge", "Fire", "Air" } },
-                { "Water", new List<string> { "Fire", "Sponge", "Gun" } },
-                { "Air", new List<string> { "Fire", "Sponge", "Lightning" } },
-                { "Sponge", new List<string> { "Water", "Gun", "Rock" } },
-                { "Fire", new List<string> { "Rock", "Sponge", "Devil" } },
-                { "Dragon", new List<string> { "Wolf", "Tree", "Snake" } },
-                { "Tree", new List<string> { "Paper", "Human", "Sponge" } },
-                { "Human", new List<string> { "Snake", "Scissors", "Gun" } },
-                { "Wolf", new List<string> { "Human", "Tree", "Paper" } },
-                { "Snake", new List<string> { "Dragon", "Wolf", "Lizard" } }
+                { "Gun", new List<string> { "Rock", "Devil", "Air", "Tree", "Wolf", "Snake", "Sponge" } },
+                { "Lightning", new List<string> { "Water", "Sponge", "Devil", "Dragon", "Human", "Tree", "Paper" } },
+                { "Devil", new List<string> { "Sponge", "Fire", "Air", "Rock", "Scissors", "Wolf", "Dragon" } },
+                { "Water", new List<string> { "Fire", "Sponge", "Gun", "Paper", "Lizard", "Snake", "Human" } },
+                { "Air", new List<string> { "Fire", "Sponge", "Lightning", "Rock", "Scissors", "Dragon", "Wolf" } },
+                { "Sponge", new List<string> { "Water", "Gun", "Rock", "Paper", "Lizard", "Tree", "Human" } },
+                { "Fire", new List<string> { "Rock", "Sponge", "Devil", "Scissors", "Snake", "Wolf", "Dragon" } },
+                { "Dragon", new List<string> { "Wolf", "Tree", "Snake", "Gun", "Air", "Lightning", "Paper" } },
+                { "Tree", new List<string> { "Paper", "Human", "Sponge", "Water", "Lizard", "Gun", "Scissors" } },
+                { "Human", new List<string> { "Snake", "Scissors", "Gun", "Rock", "Fire", "Lightning", "Dragon" } },
+                { "Wolf", new List<string> { "Human", "Tree", "Paper", "Lizard", "Air", "Gun", "Fire" } },
+                { "Snake", new List<string> { "Dragon", "Wolf", "Lizard", "Spock", "Paper", "Lightning", "Gun" } }
             };
         }
 
